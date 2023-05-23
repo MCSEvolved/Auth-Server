3f0afea63e9c7a2bb2a3761a3433c5e12a3887fc
Vincent Vermeire <vvermeire55@gmail.com>
2023-05-23 22:47:06 +0200

    Added exception catch

--- src/routes/checkUserRoles.ts
+++ src/routes/checkUserRoles.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import { Request, Response } from 'express';
-import { getAuth } from 'firebase-admin/auth';
+import { DecodedIdToken, getAuth } from 'firebase-admin/auth';
 import { setRoleOnUser } from '../utils/userUtils';
 import { checkUserIsPlayer } from '../Services/userService';
 import { Roles } from '../types';
@@ -10,7 +10,15 @@ const router = express.Router()
 
 router.post('/', bodyParser.json(), async (req: Request, res: Response) => {
     const idtoken: string = req.body.idtoken;
-    const claims = await getAuth().verifyIdToken(idtoken);
+    let claims: DecodedIdToken;
+
+    try {
+        claims = await getAuth().verifyIdToken(idtoken);
+    } catch(err: Error | any){
+        console.log('Error while verifying idtoken');
+        console.log(err?.message);
+        return res.status(400).send('Error while verifying idtoken');
+    }
     console.log('Checking roles for user: ' + claims.email);
     if (typeof claims.email === 'undefined') {
         console.log('User has no email address');
